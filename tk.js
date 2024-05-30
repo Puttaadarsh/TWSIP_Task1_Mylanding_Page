1b4e48ae785e3914df3f75cd0e559b9ab070a9fb
Putta Adarsh <puttaadarsh9539@gmail.com>
2024-05-30 23:10:08 +0530

    this is my first prj

---
+++ tk.js
@@ -0,0 +1,9 @@
+let b1=document.getElementById("cont1")
+function fun(){
+    if(b1.innerText==="CONTACT"){
+        b1.innerText="9948393995"
+    }
+    else{
+        b1.innerText="CONTACT";
+    }
+}
